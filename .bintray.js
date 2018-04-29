616e16cca29801cddda2502152bb4906e6c36b23
TheCrazyT <TheCrazyT@users.noreply.github.com>
2018-04-29 15:38:20 +0200

    fixing build

---
+++ .bintray.js
@@ -0,0 +1,24 @@
+{
+    "package": {
+        "name": "boost_python",
+        "repo": "boost_python",
+        "subject": "thecrazyt",
+        "vcs_url": "https://travis-ci.org/TheCrazyT/boost_python",
+        "licenses": ["MIT"]
+    },
+
+    "version": {
+        "name": "0.1"
+    },
+
+    "files":
+        [
+            {
+                "includePattern": "/home/travis/build/TheCrazyT/boost_python_build_win/boost_1_66_0/stage/lib/(*.dll)", "uploadPattern": "$1",
+                "matrixParams": {
+                    "override": 1 
+                }
+            }
+        ],
+    "publish": true
+}
